3fc97ab9b1c9dc1e161356f6923275111e8fad3f
beshoymichael <beshoy.michaael@gmail.com>
2025-01-14 15:14:48 +0200

    first comit

---
+++ main.js
@@ -0,0 +1,48 @@
+let searchBtn = document.querySelector("#search-btn")
+let searchForm = document.querySelector(".search-form")
+let loginForm = document.querySelector(".login-form")
+let menuBar = document.querySelector("#menu-bar")
+let amenu = document.querySelector(".navbar")
+let videoBtn = document.querySelectorAll(".video-btn")
+
+function showbar(){
+    searchBtn.classList.toggle("fa-times")
+    searchForm.classList.toggle("active")
+}
+function showform(){
+    loginForm.classList.add("active")
+}
+function hideform(){
+    loginForm.classList.remove("active")
+}
+function showmenu(){
+    menuBar.classList.toggle("fa-times")
+    amenu.classList.toggle("active")
+}
+videoBtn.forEach(slide =>{
+    slide.addEventListener("click" , function(){
+        document.querySelector(".controls .blue").classList.remove("blue")
+        slide.classList.add("blue")
+        let src = slide.getAttribute("data-src")
+        document.querySelector("#video-slider").src = src
+    })
+})
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween:20,
+    loop:true,
+    autoPlay:{
+        delay:2500
+    },
+    breakpoints:{
+        640:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        }
+    }
+});
